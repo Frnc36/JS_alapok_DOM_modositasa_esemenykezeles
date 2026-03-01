f077d9362d1f359e584846cf4083d47beca5635e
Frnc36 <magoriferenc19@gmail.com>
2026-03-01 13:37:55 +0100

    feladatat befejezve

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ window.addEventListener("load", function () {
   let lista = velSzamokGen();
   listaKiir(lista);
   formazas();
+  esemenyKezelo();
+  esemenyKezelo2();
+  esemenyKezelo4();
 });
 
 function elemekElerese1() {
@@ -49,7 +52,66 @@ function formazas() {
   szovegElem[0].classList.add("formazott");
 }
 
+/* function esemenyKezelo() {
+  const listaElem = document.querySelector(".lista");
+  listaElem.addEventListener("click", function () {
+    const kattintasutanElem = document.getElementsByClassName("kattintasutan");
+    kattintasutanElem.innerHTML = listaElem.innerHTML;
+  });
+} */
+
 function esemenyKezelo() {
-    let divElem = document.getElementsByClassName("lista");
-    divElem.addEventListener("esemeny", fvenynev)
+  let listaElem = document.querySelector(".lista");
+  listaElem.addEventListener("click", function () {
+    let kattintasutanElem = document.querySelector(".kattintasutan");
+    kattintasutanElem.innerHTML = listaElem.innerHTML;
+  });
+}
+
+function esemenyKezelo2() {
+  let feladatElem = document.querySelector(".feladat");
+  feladatElem.innerHTML = "<button>OK</button>";
+  let feladatButton = document.querySelector(".feladat button");
+  feladatButton.addEventListener("click", function () {
+    //feladatButton.innerHTML += `<div><img src="ceca.jpg" alt="Ceca" title="Ceca" class='kep-nagy'></div>`;
+    feladatElem.innerHTML += `<div><img src="ceca.jpg" alt="Ceca" title="Ceca"></div>`;
+    esemenyKezelo3();
+  });
+}
+
+/* function esemenyKezelo3() {
+    let kepElem = document.querySelector(".feladat img");
+    kepElem.addEventListener("mouseenter", function() {
+        kepElem.style.width = "300px";
+        kepElem.style.height = "auto";
+        kepElem.style.transition = "0.3s";
+    });
+    
+    kepElem.addEventListener("mouseleave", function() {
+        kepElem.style.width = "";
+        kepElem.style.height = "";
+        kepElem.style.transition = "0.3s";
+    });
+} */
+
+function esemenyKezelo3() {
+  let kepElem = document.querySelector(".feladat img");
+  kepElem.addEventListener("mouseenter", function () {
+    kepElem.classList.add("kep-nagy");
+  });
+
+  kepElem.addEventListener("mouseleave", function () {
+    kepElem.classList.remove("kep-nagy");
+  });
+}
+
+function esemenyKezelo4() {
+  let taroloElemek = document.querySelectorAll(".tarolo .elem");
+  for (let i = 0; i < taroloElemek.length; i++) {
+    taroloElemek[i].addEventListener("click", function (event) {
+      let tartalom = event.target.innerHTML;
+      let megjelenito = document.querySelector(".megjelenito");
+      megjelenito.innerHTML = tartalom;
+    });
+  }
 }
